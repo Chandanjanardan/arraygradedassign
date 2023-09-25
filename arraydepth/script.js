b6838ab82e8e76aea8103cae80a1cdb7a8167518
Chandan Janardan <chandanjanardan@gmail.com>
2023-09-25 21:37:42 +0530

    adding depth

--- arraydepth/script.js
+++ arraydepth/script.js
@@ -1,18 +1,21 @@
-function depth(arr) {
-    let depth = Array.isArray(arr) ? 1 : 0;
-    let des = [...arr];
-    while (Array.isArray(des)) {
-      for (let item of des) {
-        if (Array.isArray(item)) {
-          des = [...item];
-          depth++;
-          break;
-        } else {
-          des = item;
-        }
-      }
-    }
-    return depth;
-  }
-  console.log(depth(([1, [2, [3, 4]]]) ))
-  console.log(depth([1,2,3,4]))
+// function depth(arr) {
+//     let depth = Array.isArray(arr) ? 1 : 0;
+//     let des = [...arr];
+//     while (Array.isArray(des)) {
+//       for (let item of des) {
+//         if (Array.isArray(item)) {
+//           des = [...item];
+//           depth++;
+//           break;
+//         } else {
+//           des = item;
+//         }
+//       }
+//     }
+//     return depth;
+//   }
+//   console.log(depth(([1, [2, [3, 4]]]) ))
+//   console.log(depth([1,2,3,4]))
+function depth(aee){
+  let depth = Array.isArray(Array)
+}
